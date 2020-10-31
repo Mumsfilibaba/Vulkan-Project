2785dc7c94645fb4ffecb2a8fc15d5b993608638
Alexander Dahlin <alexander.dahlins@gmail.com>
2020-10-31 17:13:52 +0100

    Update Camera.h

--- VulkanProject/src/Renderer/Camera.h
+++ VulkanProject/src/Renderer/Camera.h
@@ -7,7 +7,13 @@ public:
 	Camera() = default;
 	~Camera() = default;
 	
-	glm::mat4 GetMatrix() const
+	inline void Update()
+	{
+		m_View = glm::lookAtLH(glm::vec3(0.0f, 0.0f, -2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+		//m_Projection = glm::perspectiveFovLH();
+	}
+
+	inline glm::mat4 GetMatrix() const
 	{
 		return m_Projection * m_View;
 	}
@@ -15,4 +21,7 @@ public:
 private:
 	glm::mat4 m_View 		= glm::identity<glm::mat4>();
 	glm::mat4 m_Projection 	= glm::identity<glm::mat4>();
+	
+	float m_FieldOfView = glm::pi<float>() / 2.0f;
+
 };
