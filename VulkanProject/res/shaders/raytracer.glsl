f0c676c69970b0ea950faede7e754cfffc413e74
Alexander Dahlin <alexander.dahlins@gmail.com>
2024-07-23 11:41:21 +0200

    Refactored debug-mode

--- VulkanProject/res/shaders/raytracer.glsl
+++ VulkanProject/res/shaders/raytracer.glsl
@@ -299,7 +299,7 @@ FHitInfo HitTriangle(in vec3 Vertex0, in vec3 Vertex1, in vec3 Vertex2, in vec3 
     return HitInfo;
 }
 
-void HitMesh(uint RootBoxIndex, in FRay Ray, inout FRayPayLoad PayLoad, uint MaterialIndex)
+void HitMesh(uint RootBoxIndex, in FRay Ray, inout FRayPayLoad PayLoad, uint MaterialIndex, inout ivec2 Stats)
 {
     // Create a stack for checking all the nodes
     const uint MaxDepth = BVH_MAX_DEPTH;
@@ -329,7 +329,9 @@ void HitMesh(uint RootBoxIndex, in FRay Ray, inout FRayPayLoad PayLoad, uint Mat
                 vec3 Position1 = Vertices[Triangle.Index1].Position.xyz;
                 vec3 Position2 = Vertices[Triangle.Index2].Position.xyz;
 
-                FHitInfo HitInfo = HitTriangle(Position0, Position1, Position2, Ray.Origin, Ray.Direction); 
+                FHitInfo HitInfo = HitTriangle(Position0, Position1, Position2, Ray.Origin, Ray.Direction);
+                Stats[1]++;
+
                 if (HitInfo.Dist > PayLoad.MinT && HitInfo.Dist < PayLoad.MaxT && HitInfo.Dist < PayLoad.T)
                 {
                     PayLoad.T = HitInfo.Dist;
@@ -362,6 +364,7 @@ void HitMesh(uint RootBoxIndex, in FRay Ray, inout FRayPayLoad PayLoad, uint Mat
             // Check intersection of child nodes
             float Dist1 = IntersectRayAABB(BvhNodes[ChildIndex1].BoxMinAndIndex.xyz, BvhNodes[ChildIndex1].BoxMaxAndNumTriangles.xyz, Ray.Origin, Ray.InvDirection);
             float Dist2 = IntersectRayAABB(BvhNodes[ChildIndex2].BoxMinAndIndex.xyz, BvhNodes[ChildIndex2].BoxMaxAndNumTriangles.xyz, Ray.Origin, Ray.InvDirection);
+            Stats[0] += 2;
 
             // Ensure 1 is the closest
             if (Dist1 > Dist2)
@@ -400,7 +403,7 @@ void HitMesh(uint RootBoxIndex, in FRay Ray, inout FRayPayLoad PayLoad, uint Mat
     }
 }
 
-bool TraceRay(in FRay Ray, inout FRayPayLoad PayLoad)
+bool TraceRay(in FRay Ray, inout FRayPayLoad PayLoad, inout ivec2 Stats)
 {
     for (uint i = 0; i < uScene.NumSpheres; i++)
     {
@@ -417,7 +420,7 @@ bool TraceRay(in FRay Ray, inout FRayPayLoad PayLoad)
     for (uint i = 0; i < uScene.NumMeshes; i++)
     {
         FMesh Mesh = Meshes[i];
-        HitMesh(Mesh.BoundingBoxIndex, Ray, PayLoad, Mesh.MaterialIndex);
+        HitMesh(Mesh.BoundingBoxIndex, Ray, PayLoad, Mesh.MaterialIndex, Stats);
     }
 
     return PayLoad.T < PayLoad.MaxT;
@@ -509,6 +512,9 @@ vec3 GetColorForRay(in FRay Ray, inout uint RandomSeed)
     vec3 RayColor    = vec3(1.0);
     vec3 SampleColor = vec3(0.0);
 
+    // Stats: x=NumBoxTests, y=NumTriangleTests
+    ivec2 Stats;
+
     // Add one bounce (Primary ray)
     uint MaxBounces = min(uScene.NumBounces, MAX_DEPTH) + 1;
     for (uint i = 0; i < MaxBounces; i++)
@@ -520,7 +526,7 @@ vec3 GetColorForRay(in FRay Ray, inout uint RandomSeed)
         PayLoad.bFrontFace  = false;
         PayLoad.bFromInside = false;
 
-        if (TraceRay(Ray, PayLoad))
+        if (TraceRay(Ray, PayLoad, Stats))
         {
             const uint MaterialIndex = min(PayLoad.MaterialIndex, uScene.NumMaterials - 1);
             FMaterial Material = Materials[MaterialIndex];
@@ -666,7 +672,9 @@ vec3 GetNormalForRay(in FRay Ray)
     PayLoad.bFrontFace  = false;
     PayLoad.bFromInside = false;
 
-    if (TraceRay(Ray, PayLoad))
+    // Stats: x=NumBoxTests, y=NumTriangleTests
+    ivec2 Stats;
+    if (TraceRay(Ray, PayLoad, Stats))
     {
         const uint MaterialIndex = min(PayLoad.MaterialIndex, uScene.NumMaterials - 1);
 
@@ -708,7 +716,9 @@ vec3 GetBarycentricsForRay(in FRay Ray)
     PayLoad.bFrontFace  = false;
     PayLoad.bFromInside = false;
 
-    if (TraceRay(Ray, PayLoad))
+    // Stats: x=NumBoxTests, y=NumTriangleTests
+    ivec2 Stats;
+    if (TraceRay(Ray, PayLoad, Stats))
     {
         return PayLoad.BaryCentrics;
     }
@@ -727,7 +737,9 @@ vec3 GetTexCoordsForRay(in FRay Ray)
     PayLoad.bFrontFace  = false;
     PayLoad.bFromInside = false;
 
-    if (TraceRay(Ray, PayLoad))
+    // Stats: x=NumBoxTests, y=NumTriangleTests
+    ivec2 Stats;
+    if (TraceRay(Ray, PayLoad, Stats))
     {
         return vec3(PayLoad.TexCoords, 0.0);
     }
@@ -746,7 +758,9 @@ vec3 GetAlbedoForRay(in FRay Ray)
     PayLoad.bFrontFace  = false;
     PayLoad.bFromInside = false;
 
-    if (TraceRay(Ray, PayLoad))
+    // Stats: x=NumBoxTests, y=NumTriangleTests
+    ivec2 Stats;
+    if (TraceRay(Ray, PayLoad, Stats))
     {
         const uint MaterialIndex = min(PayLoad.MaterialIndex, uScene.NumMaterials - 1);
 
@@ -768,15 +782,6 @@ vec3 GetAlbedoForRay(in FRay Ray)
 
 vec3 GetColorForRay_BvhDebug(in FRay Ray)
 {
-    // Create a stack for checking all the nodes
-    const uint MaxDepth = BVH_MAX_DEPTH;
-    uint Stack[MaxDepth];
-
-    // Initialize the stack to visit the rootnode first
-    int StackIndex = 0;
-    Stack[StackIndex] = BVH_ROOT_NODE_INDEX;
-
-    // Payload
     FRayPayLoad PayLoad;
     PayLoad.MinT        = 0.0001;
     PayLoad.MaxT        = 100000.0;
@@ -784,66 +789,12 @@ vec3 GetColorForRay_BvhDebug(in FRay Ray)
     PayLoad.bFrontFace  = false;
     PayLoad.bFromInside = false;
 
-    // Start go through all the nodes
-    uint NumBoxTests      = 0;
-    uint NumTriangleTests = 0;
-    while (StackIndex >= 0)
-    {
-        // Pop the stack
-        const uint NodeIndex = Stack[StackIndex];
-        StackIndex--;
+    // Stats: x=NumBoxTests, y=NumTriangleTests
+    ivec2 Stats = ivec2(0, 0);
+    TraceRay(Ray, PayLoad, Stats);
 
-        // Check if we hit this node
-        FBoundingBox Node = BvhNodes[NodeIndex];
-        if (floatBitsToUint(Node.BoxMaxAndNumTriangles.w) > 0)
-        {
-            uint LastTriangleIndex = floatBitsToUint(Node.BoxMinAndIndex.w) + floatBitsToUint(Node.BoxMaxAndNumTriangles.w);
-            for (uint TriangleIndex = floatBitsToUint(Node.BoxMinAndIndex.w); TriangleIndex < LastTriangleIndex; TriangleIndex++)
-            {
-                FTriangle Triangle = Triangles[TriangleIndex];
-                vec3 Position0 = Vertices[Triangle.Index0].Position.xyz;
-                vec3 Position1 = Vertices[Triangle.Index1].Position.xyz;
-                vec3 Position2 = Vertices[Triangle.Index2].Position.xyz;
-
-                FHitInfo HitInfo = HitTriangle(Position0, Position1, Position2, Ray.Origin, Ray.Direction); 
-                if (HitInfo.Dist > PayLoad.MinT && HitInfo.Dist < PayLoad.MaxT && HitInfo.Dist < PayLoad.T)
-                {
-                    PayLoad.T = HitInfo.Dist;
-                }
-
-                NumTriangleTests++;
-            }
-        }
-        else
-        {
-            uint ChildIndex1 = floatBitsToUint(Node.BoxMinAndIndex.w);
-            uint ChildIndex2 = floatBitsToUint(Node.BoxMinAndIndex.w) + 1;
-
-            // Check intersection of child nodes
-            float Dist1 = IntersectRayAABB(BvhNodes[ChildIndex1].BoxMinAndIndex.xyz, BvhNodes[ChildIndex1].BoxMaxAndNumTriangles.xyz, Ray.Origin, Ray.InvDirection);
-            float Dist2 = IntersectRayAABB(BvhNodes[ChildIndex2].BoxMinAndIndex.xyz, BvhNodes[ChildIndex2].BoxMaxAndNumTriangles.xyz, Ray.Origin, Ray.InvDirection);
-            NumBoxTests += 2;
-
-            // Ensure 1 is the closest
-            if (Dist1 > Dist2)
-            {
-                if (Dist1 < PayLoad.T)
-                    Stack[++StackIndex] = ChildIndex1;
-                if (Dist2 < PayLoad.T)
-                    Stack[++StackIndex] = ChildIndex2;
-            }
-            else
-            {
-                if (Dist2 < PayLoad.T)
-                    Stack[++StackIndex] = ChildIndex2;
-                if (Dist1 < PayLoad.T)
-                    Stack[++StackIndex] = ChildIndex1;
-            }
-        }
-    }
-
-    vec3 BoxTestColor      = vec3(float(NumBoxTests)) / 50.0;
-    vec3 TriangleTestColor = vec3(float(NumTriangleTests)) / 50.0;
+    vec3 BoxTestColor      = vec3(float(Stats[0])) / 50.0;
+    vec3 TriangleTestColor = vec3(float(Stats[1])) / 50.0;
     return BoxTestColor;
 }
 
